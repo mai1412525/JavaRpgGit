96cd7e91d2e538028fc74d609d1eb7b388d99f41
b231029 <b231029@A1048.kca.local>
2024-01-11 11:13:16 +0900

    あぽむ:MapMamの追加

---
+++ Play2/src/play/MapMam.java
@@ -0,0 +1,9 @@
+package play;
+
+public abstract class MapMam {
+	public abstract int[][] map();
+	public abstract int[]getmapjouhou(int i);
+	public abstract boolean gethantei();
+	public abstract String getname();
+	public abstract int[][]ivent();
+}
