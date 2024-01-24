d07f7802d389b8cb1cb000d0fab55b21335ba123
まい <88076077+mai1412525@users.noreply.github.com>
2024-01-24 14:11:06 +0900

    メインイベント作った

---
+++ Play2/src/play/MainEvent.java
@@ -0,0 +1,6 @@
+package play;
+Class MainEvent{
+  public static int[] flag = new int[8];//フラグ管理
+	public static int[] item =  new int[15];//アイテム管理
+	public static int[] Epissave =  new int[100];//Epiソードどこまで終わったか管理
+}
