5ea8a4681d6a286853b343c099e090c9add3a2d8
まい <88076077+mai1412525@users.noreply.github.com>
2024-01-24 14:14:53 +0900

    Delete Play2/src/play/MainEvent.java

--- Play2/src/play/MainEvent.java
+++
@@ -1,6 +0,0 @@
-package play;
-Class MainEvent{
-  public static int[] flag = new int[8];//フラグ管理
-	public static int[] item =  new int[15];//アイテム管理
-	public static int[] Epissave =  new int[100];//Epiソードどこまで終わったか管理
-}
