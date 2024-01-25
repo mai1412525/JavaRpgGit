498216690f6ccac7c7a8357027d3b9bd6a4bf74c
B231034 <B231034@A1047.kca.local>
2024-01-25 12:18:24 +0900

    MAP変更

--- Play2/src/play/Map2.java
+++ Play2/src/play/Map2.java
@@ -59,13 +59,52 @@ class rikajunbishitu extends MapMam{
 	int[][]syougai = new int[tate][yoko];
 	int[]kuromasu = {3,3,4,3,5,3,6,3,7,3,8,3,9,3,4,5,4,6,4,7,4,8,4,9,3,11,4,11,5,11,6,11,7,11,8,11,9,11,7,6,7,7,7,8,8,6,8,7,8,8,
 			9,6,9,7,9,8,11,2,11,3,12,2,12,3,11,11,11,12,12,11,12,12};
-	int[]kuti = {-31,1,2,3,4,5,6,7,8,9,10};
+	int[]kuti = {-31,1,2,-32,4,5};
 	int[]jouhou = {};
 
 	int[]mituketa = new int[3];
 	int[][]ivent = new int[tate][yoko];
 
 	String name = "理科室準備室";
+	MapSyori syori = new MapSyori();
+
+	public int[][] map(){
+		syori.sotowaku(syougai);
+		syori.jouh(syougai,kuromasu,1);
+		syori.kuti(syougai, kuti);
+		return syougai;
+	}
+
+	public int[] getmapjouhou(int id) {
+		syori.mapjouhou(id, jouhou, mituketa);
+		return mituketa;
+	}
+	public String getname() {
+		return name;
+	}
+	public int[][] ivent(){
+		return ivent;
+	}
+	public void oru(int ii,int t) {
+		for(int i = 0; i < ivent.length; i++) {
+			for(int j = 0; j < ivent[1].length; j++) {
+				if(ivent[i][j] == ii) {
+					ivent[i][j] = t;
+				}
+			}
+		}
+	}
+}
+class rouka_joubu extends MapMam{
+	int tate = 8;
+	int yoko = 90;
+	int[][]syougai = new int[tate][yoko];
+	int[]kuromasu = {};
+	int[]kuti = {-41,3,4,-12,1,2,-13,11,12,-14,26,27,-15,42,43,-16,59,60,-17,75,76,-21,4,5,-31,11,12,-32,22,23,-33,43,44,-34,61,62,-35,1,2,3,4,5,6,7,-35,71,72,73,74,75,76,77};
+	int[]jouhou = {};
+	int[]mituketa = new int[3];
+	int[][]ivent = new int[tate][yoko];
+	String name = "廊下";
 	MapSyori syori = new MapSyori();
 
 	public int[][] map(){
